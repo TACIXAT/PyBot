579c9b8f2b4b2b0ec8613a23914e85073f2161ad
dgoddard <edwardhowl@gmail.com>
2013-01-18 12:22:54 -1000

    Added sendModified for Windows.

--- robot.py
+++ robot.py
@@ -1,29 +1,31 @@
 import sys
 from time import sleep
+try:
+	import win32com.client
+except ImportError:
+	import Xlib.display
+	import Xlib.ext.xtest as xtest
+	import Xlib.XK
 
 class robot:	
 	opsys = sys.platform
 
 	if opsys.find('linux') != -1:
 		def __init__(self):
-			import Xlib.display
-			import Xlib.ext.xtest as xtest
-			import Xlib.XK
-
 			self.keyboard = {
-			'`':'`', '1':'1', '2':'2', '3':'3', '4':'4', '5':'5', '6':'6', '7':'7', '8':'8', '9':'9', '0':'0', '-':'minus', '=':'equal',
-			'q':'q', 'w':'w', 'e':'e', 'r':'r', 't':'t', 'y':'y', 'u':'u', 'i':'i', 'o':'o', 'p':'p', '[':'braceleft', ']':'braceright', '\\':'backslash', 
-			'a':'a', 's':'s', 'd':'d', 'f':'f', 'g':'g', 'h':'h', 'j':'j', 'k':'k', 'l':'l', ';':'semicolon', '\'':'apostrophe', 
-			'z':'z', 'x':'x', 'c':'c', 'v':'v', 'b':'b', 'n':'n', 'm':'m', ',':'comma', '.':'period', '/':'slash', 
-			'~':['Shift_L', '`'], '!':['Shift_L', '1'], '@':['Shift_L', '2'], '#':['Shift_L', '3'], '$':['Shift_L', '4'], '%':['Shift_L', '5'], '^':['Shift_L', '6'], '&':['Shift_L', '7'], '*':['Shift_L', '8'], '(':['Shift_L', '9'], ')':['Shift_L', '0'], '_':['Shift_L', 'minus'], '+':['Shift_L', 'equal'], 
-			'Q':['Shift_L', 'q'], 'W':['Shift_L', 'w'], 'E':['Shift_L', 'e'], 'R':['Shift_L', 'r'], 'T':['Shift_L', 't'], 'Y':['Shift_L', 'y'], 'U':['Shift_L', 'u'], 'I':['Shift_L', 'i'], 'O':['Shift_L', 'o'], 'P':['Shift_L', 'p'], '{':['Shift_L','braceleft'], '}':['Shift_L','braceright'], '|':['Shift_L', 'backslash'], 
-			'A':['Shift_L', 'a'], 'S':['Shift_L', 's'], 'D':['Shift_L', 'd'], 'F':['Shift_L', 'f'], 'G':['Shift_L', 'g'], 'H':['Shift_L', 'h'], 'J':['Shift_L', 'j'], 'K':['Shift_L', 'k'], 'L':['Shift_L', 'l'], ':':['Shift_L', 'semicolon'], '"':['Shift_L', "apostrophe"], 
-			'Z':['Shift_L', 'z'], 'X':['Shift_L', 'x'], 'C':['Shift_L', 'c'], 'V':['Shift_L', 'v'], 'B':['Shift_L', 'b'], 'N':['Shift_L', 'n'], 'M':['Shift_L', 'm'], '<':['Shift_L', 'comma'], '>':['Shift_L', 'period'], '?':['Shift_L', 'slash'],
-			' ':'space', '\n':'Return', '\t':'Tab', 
-			'\x00':'leftarrow', '\x01':'uparrow', '\x02':'rightarrow', '\x03':'downarrow', '\x04':'Shift_L', '\x05':'Shift_R', '\x06':'Control_L', '\x07':'Control_R', 
-			'\x08':'Backspace', '\x09':'Tab', '\x0A':'Return', '\x0B':'vt', '\x0C':'ff', '\x0D':'cr', '\x0E':'Alt_L', '\x0F':'Alt_R', 
-			'\x10':'Super_L', '\x11':'Super_R', '\x12':'Menu', '\x13':'Insert', '\x14':'Scroll_Lock', '\x15':'Page_Up', '\x16':'Page_Down', '\x17':'Num_Lock', 
-			'\x18':'Pause', '\x19':'Printscreen', '\x1A':'F1', '\x1B':'Escape', '\x1C':'Delete', '\x1D':'Home', '\x1E':'End', '\x1F':'F12', }
+				'`':'`', '1':'1', '2':'2', '3':'3', '4':'4', '5':'5', '6':'6', '7':'7', '8':'8', '9':'9', '0':'0', '-':'minus', '=':'equal',
+				'q':'q', 'w':'w', 'e':'e', 'r':'r', 't':'t', 'y':'y', 'u':'u', 'i':'i', 'o':'o', 'p':'p', '[':'braceleft', ']':'braceright', '\\':'backslash', 
+				'a':'a', 's':'s', 'd':'d', 'f':'f', 'g':'g', 'h':'h', 'j':'j', 'k':'k', 'l':'l', ';':'semicolon', '\'':'apostrophe', 
+				'z':'z', 'x':'x', 'c':'c', 'v':'v', 'b':'b', 'n':'n', 'm':'m', ',':'comma', '.':'period', '/':'slash', 
+				'~':['Shift_L', '`'], '!':['Shift_L', '1'], '@':['Shift_L', '2'], '#':['Shift_L', '3'], '$':['Shift_L', '4'], '%':['Shift_L', '5'], '^':['Shift_L', '6'], '&':['Shift_L', '7'], '*':['Shift_L', '8'], '(':['Shift_L', '9'], ')':['Shift_L', '0'], '_':['Shift_L', 'minus'], '+':['Shift_L', 'equal'], 
+				'Q':['Shift_L', 'q'], 'W':['Shift_L', 'w'], 'E':['Shift_L', 'e'], 'R':['Shift_L', 'r'], 'T':['Shift_L', 't'], 'Y':['Shift_L', 'y'], 'U':['Shift_L', 'u'], 'I':['Shift_L', 'i'], 'O':['Shift_L', 'o'], 'P':['Shift_L', 'p'], '{':['Shift_L','braceleft'], '}':['Shift_L','braceright'], '|':['Shift_L', 'backslash'], 
+				'A':['Shift_L', 'a'], 'S':['Shift_L', 's'], 'D':['Shift_L', 'd'], 'F':['Shift_L', 'f'], 'G':['Shift_L', 'g'], 'H':['Shift_L', 'h'], 'J':['Shift_L', 'j'], 'K':['Shift_L', 'k'], 'L':['Shift_L', 'l'], ':':['Shift_L', 'semicolon'], '"':['Shift_L', "apostrophe"], 
+				'Z':['Shift_L', 'z'], 'X':['Shift_L', 'x'], 'C':['Shift_L', 'c'], 'V':['Shift_L', 'v'], 'B':['Shift_L', 'b'], 'N':['Shift_L', 'n'], 'M':['Shift_L', 'm'], '<':['Shift_L', 'comma'], '>':['Shift_L', 'period'], '?':['Shift_L', 'slash'],
+				' ':'space', '\n':'Return', '\t':'Tab', 
+				'\x00':'leftarrow', '\x01':'uparrow', '\x02':'rightarrow', '\x03':'downarrow', '\x04':'Shift_L', '\x05':'Shift_R', '\x06':'Control_L', '\x07':'Control_R', 
+				'\x08':'Backspace', '\x09':'Tab', '\x0A':'Return', '\x0B':'vt', '\x0C':'ff', '\x0D':'cr', '\x0E':'Alt_L', '\x0F':'Alt_R', 
+				'\x10':'Super_L', '\x11':'Super_R', '\x12':'Menu', '\x13':'Insert', '\x14':'Scroll_Lock', '\x15':'Page_Up', '\x16':'Page_Down', '\x17':'Num_Lock', 
+				'\x18':'Pause', '\x19':'Printscreen', '\x1A':'F1', '\x1B':'Escape', '\x1C':'Delete', '\x1D':'Home', '\x1E':'End', '\x1F':'F12', }
 
 			#basic set of free chars needed to not interfere with python escapes (can't find BEL char \x07)
 			#mappings are found in file: cat /usr/include/X11/keysymdef.h | grep -i KEYNAME
@@ -62,7 +64,6 @@ class robot:
 
 	elif opsys.find('win32') != -1:
 		def __init__(self):
-			import win32com.client
 			self.shell = win32com.client.Dispatch("WScript.Shell")
 			
 			self.keyboard = {
@@ -82,6 +83,17 @@ class robot:
 			
 		def sendSingle(self, char):
 			self.shell.SendKeys(char)
+
+		def sendModified(self, char, modifiers):
+			modList = [1,2,4]
+			lookup = {0:'', 1:'{LCTRL}', 2:'{LSHIFT}', 4:'{LALT}'}
+			sendStr = ''
+
+			for modMask in modList:
+				sendStr += lookup[modifiers&mod]
+
+			sendStr += keyboard[char]
+			self.sendSingle(sendStr)
 			
 
 	'''os independent methods'''
@@ -107,4 +119,10 @@ username = 'CAPSBOT@gmail.com'
 password = 'hunter2$MONEY!'
 kittens.typeOut(username + '\n')
 kittens.typeOut(password + '\n')
+sleep(0.5)
+kittens.sendModified('f',1)
+sleep(0.5)
+kittens.typeOut(username)
+sleep(0.5)
+kittens.sendModified('f',1|2)
 quit()
